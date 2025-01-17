f5754d1d050442f60509d745976116a00ea2e12b
Chen-Renchang <3350727975@qq.com>
2025-01-17 08:03:18 +0300

    15

--- app/db/models/group.py
+++ app/db/models/group.py
@@ -3,25 +3,27 @@ from uuid import UUID, uuid4
 
 
 class GroupModel(SQLModel, table=True):
-    __tablename__ = "groups"
+    """Модель группы."""
+    __tablename__ = "groups"  # Название таблицы в базе данных
 
-    id: UUID = Field(primary_key=True)
-    name: str
-    group_number: str
-
+    id: UUID = Field(primary_key=True)  # Уникальный идентификатор группы (первичный ключ)
+    name: str  # Название группы
+    group_number: str  # Номер группы
 
 
 class StudentModel(SQLModel, table=True):
-    __tablename__ = "students"
+    """Модель студента."""
+    __tablename__ = "students"  # Название таблицы в базе данных
 
-    id: UUID = Field(primary_key=True)
-    name: str
-    student_number: str
+    id: UUID = Field(primary_key=True)  # Уникальный идентификатор студента (первичный ключ)
+    name: str  # Имя студента
+    student_number: str  # Номер студента
 
 
 class GroupStudentModel(SQLModel, table=True):
-    __tablename__ = "group_students"
+    """Модель для связи студентов и групп."""
+    __tablename__ = "group_students"  # Название таблицы в базе данных
 
-    id: UUID = Field(primary_key=True, default_factory=uuid4)
-    group_id: UUID = Field(foreign_key="groups.id")
-    student_id: UUID = Field(foreign_key="students.id")
+    id: UUID = Field(primary_key=True, default_factory=uuid4)  # Уникальный идентификатор связи (первичный ключ)
+    group_id: UUID = Field(foreign_key="groups.id")  # Внешний ключ на таблицу групп
+    student_id: UUID = Field(foreign_key="students.id")  # Внешний ключ на таблицу студентов
