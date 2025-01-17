18663d892a21113485254bbefff1258a8041c8d9
Chen-Renchang <3350727975@qq.com>
2025-01-17 08:02:28 +0300

    14

--- app/core/db_config.py
+++ app/core/db_config.py
@@ -3,16 +3,11 @@ from sqlmodel import create_engine
 
 from app.core.config import SETTINGS
 
+# Вывод настроек для проверки
 print(SETTINGS)
-#
-# DB_URI = PostgresDsn.build(
-#     scheme='postgresql',
-#     user=SETTINGS.DB_USER,
-#     password=SETTINGS.DB_PASSWORD,
-#     host=f"{SETTINGS.DB_HOSTNAME}:{SETTINGS.DB_PORT}",
-#     path=f"/{SETTINGS.DB_NAME}"
-# )
 
+# Создание URI для подключения к PostgreSQL
 DB_URI = f"postgresql://{SETTINGS.DB_USER}:{SETTINGS.DB_PASSWORD}@{SETTINGS.DB_HOSTNAME}:{SETTINGS.DB_PORT}/{SETTINGS.DB_NAME}"
 
+# Создание движка базы данных с использованием SQLModel
 DB_ENGINE = create_engine(DB_URI)
