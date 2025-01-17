3f09aff6d47c9bd57871df72a3beb4d5e519708e
Chen-Renchang <3350727975@qq.com>
2025-01-17 08:00:04 +0300

    13

--- app/core/dependencies/db_session.py
+++ app/core/dependencies/db_session.py
@@ -4,5 +4,6 @@ from app.core.db_config import DB_ENGINE
 
 
 def session_dependency():
+    """Зависимость для создания сессии базы данных."""
     with Session(DB_ENGINE) as session:
         yield session
